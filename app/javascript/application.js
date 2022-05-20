3c3fb78d3d1990f7af46c85440333ecdd855edfa
Anne Richardson <richardson.ae@gmail.com>
2022-05-20 13:22:03 -0500

    Mess around with Turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+
+// Uncomment to disable Turbo in whole app
+// import { Turbo } from "@hotwired/turbo-rails"
+// Turbo.session.drive = false

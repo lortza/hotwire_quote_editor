05a79d4a29f41dee5cf920b088a1d04d8b262e80
Anne Richardson <richardson.ae@gmail.com>
2022-10-17 15:29:23 -0500

    Ch 6: add home page and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "quotes#index"
+  root to: "pages#home"
   resources :quotes
 end

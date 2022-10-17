c5aa53ec0daea63da16f91a01838cff0ac37259a
Anne Richardson <richardson.ae@gmail.com>
2022-10-17 14:31:32 -0500

    Ch 6: add company model and data

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_18_233713) do
+ActiveRecord::Schema.define(version: 2022_10_17_192542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.string "name", null: false
